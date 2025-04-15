2bdc26618e530f35216865c1670ac0ef958be1e1
Muhaimin Khan <khnmuhaimin@gmail.com>
2025-04-15 21:33:12 +0200

    Created endpoints (incomplete) for starting registration and getting registration tasks

---
+++ routes/weighing_nodes.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, jsonify, request
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from auth.auth import generate_secret, hash_secret, verify_preshared_key
+from database.models.registration_task import RegistrationTask
+from database.database import database_engine
+
+
+weighing_node_blueprint = Blueprint("weighing_node", __name__, url_prefix='/weighing-nodes')
+
+
+@weighing_node_blueprint.route("/registration/start", methods=["POST"])
+def start_registration():
+    # auth_header = request.headers.get("Authorization")
+    # correct_key = verify_preshared_key(auth_header)
+    # if not correct_key:
+    #     return ("Invalid key.", 401)
+    with Session(database_engine) as session:
+        client_ip_address = request.remote_addr
+        task = RegistrationTask(ip_address=client_ip_address)
+        task.api_key = generate_secret()
+        task.hashed_api_key = hash_secret(task.api_key)
+        session.add(task)
+        session.commit()
+        uuid = task.task_id
+        hashed_api_key = task.hashed_api_key
+    # TODO: alert admin
+    return (f"{uuid}\n{hashed_api_key}\n", 200)
+
+
+@weighing_node_blueprint.route("/registration/tasks")
+def get_registration_tasks():
+    # TODO: do admin auth
+    with Session(database_engine) as session:
+        tasks = session.scalars(select(RegistrationTask)).all()
+    tasks = list(map(lambda t: t.admin_view(), tasks))
+    tasks.sort(key=lambda t: t["created_at"])
+    return jsonify(tasks)
+
+
+    
