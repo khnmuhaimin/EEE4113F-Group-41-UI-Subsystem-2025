02f80a861f63660eed0b3aef4d1f35a0f797bb5c
Muhaimin Khan <khnmuhaimin@gmail.com>
2025-04-16 14:45:53 +0200

    Generalized utc timestamp to accept an offset

--- database/utils/utils.py
+++ database/utils/utils.py
@@ -1,11 +1,14 @@
 from datetime import datetime, timezone
 
 
-def utc_timestamp() -> int:
+def utc_timestamp(offset: int = 0) -> int:
     """
-    Returns the current UTC timestamp as an integer.
+    Returns the current UTC timestamp as an integer, adjusted by an optional offset.
+
+    Args:
+        offset (int, optional): The number of seconds to adjust the UTC timestamp. Default is 0.
 
     Returns:
-        int: The current UTC time in seconds since the Unix epoch.
+        int: The current UTC time in seconds since the Unix epoch, adjusted by the provided offset.
     """
-    return round(datetime.now(tz=timezone.utc).timestamp())
+    return round(datetime.now(tz=timezone.utc).timestamp()) + offset
