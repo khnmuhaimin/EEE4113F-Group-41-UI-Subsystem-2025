d183de095ad2894fce5d5598d498a65ee4d0fada
Muhaimin Khan <khnmuhaimin@gmail.com>
2025-04-19 07:48:11 +0200

    Added function to validate ip addresses

--- backend/database/utils/utils.py
+++ backend/database/utils/utils.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timezone
+import ipaddress
 
 
 def utc_timestamp(offset: int = 0) -> int:
@@ -12,3 +13,21 @@ def utc_timestamp(offset: int = 0) -> int:
         int: The current UTC time in seconds since the Unix epoch, adjusted by the provided offset.
     """
     return round(datetime.now(tz=timezone.utc).timestamp()) + offset
+
+
+def is_ip_address(ip_string: str) -> bool:
+    """
+    Check if the provided string is a valid IP address.
+
+    Args:
+        ip_string (str): The string to validate.
+
+    Returns:
+        bool: True if the string is a valid IPv4 or IPv6 address, False otherwise.
+    """
+    try:
+        ipaddress.ip_address(ip_string)
+        return True
+    except:
+        return False
+    
