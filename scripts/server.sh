db3a5c027ec0da67a58e21a2ed0ec269d9d67911
Muhaimin Khan <khnmuhaimin@gmail.com>
2025-04-28 01:51:28 +0200

    Improved the server bash scripts

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 setup_backend_venv() {
-    local venv_dir="${PROJECT_DIR}"/backend/.venv
-    local requirements_file="${PROJECT_DIR}"/backend/requirements.txt
+    local venv_dir="$PROJECT_DIR"/backend/.venv
+    local requirements_file="$PROJECT_DIR"/backend/requirements.txt
 
     if [[ ! -d "$venv_dir" ]]; then
         if python3 -m venv "$venv_dir"; then
@@ -19,38 +19,21 @@ setup_backend_venv() {
         fail "Couldn't activate the python virtual environment for the backend."
         exit 1
     fi
-    # Install dependencies quietly
-    if [[ -f "$requirements_file" ]]; then
-        pip install -r "$requirements_file" -qq
-    else
-        fail "Requirements file not found: $requirements_file"
+
+    if [[ ! -f "$PROJECT_DIR"/requirements.txt ]]; then
+        fail "Couldn't find the dependency list at $PROJECT_DIR/requirements.txt."
         exit 1
     fi
-    return 0
+    
+    dependencies="$(pip freeze)"
+    installed="$(cat "$PROJECT_DIR"/requirements.txt)"
+    if [[ "$dependencies" != "$installed" ]]; then
+        log "Installing dependencies."
+        pip install -r "$requirements_file" -qq
+    fi
 }
 
 
-# Description:
-#   Starts the server process by setting up the Python virtual environment,
-#   navigating to the backend directory, and running the `gunicorn` server.
-#   If the server is already running, it skips the operation. The function
-#   handles logging and returns an appropriate status code based on success or failure.
-#
-# Arguments:
-#   None
-#
-# Global Variables:
-#   SERVER_PROCESS_TAG - Tag used to identify the server process.
-#   RUNNING - Global variable set by `check_process` to indicate if the process is running.
-#   TRUE - Constant representing "true" used for process check comparison.
-#   ENVIRONMENT - Environment variable that determines the environment (e.g., DEVELOPMENT).
-#   LOG_PATH - Path to the log file for the server.
-#   PROJECT_DIR - The root project directory.
-#   SERVER_PORT - The port number to bind the server to.
-#
-# Return Values:
-#   0 - If the server was started successfully.
-#   1 - If there was an issue starting the server.
 start_server() {
 
     # noop if the server is running
@@ -60,8 +43,10 @@ start_server() {
         return 0
     fi
 
-    local current_dir
-    current_dir="$(pwd)"  # jump back at the end of the function
+    local original_dir
+    original_dir="$(pwd)"  # jump back at the end of the function
+    trap 'cd $original_dir' EXIT
+
 
     # start the server
     setup_backend_venv || exit 1
@@ -77,11 +62,9 @@ start_server() {
     if start_process "$SERVER_PROCESS_TAG" "$start_command"; then
         log "Started the server."
         log "See $LOG_PATH for server logs."
-        cd "$current_dir" || exit 1
         return 0
     else
         fail "Something went wrong when starting the server."
-        cd "$current_dir" || exit 1
         return 1
     fi
 }
@@ -97,7 +80,7 @@ stop_server() {
 
     # kill the server
     if stop_process "$SERVER_PROCESS_TAG"; then
-        log "The server was shut down."
+        log "The server is shutting down. Check the logs to check if the shutdown is complete."
     else
         warn "Could not shut down the server. Perform the shut down manually."
     fi
