ba5d4065a3a13ef7d9178b39450b7f265c8e884f
Muhaimin Khan <khnmuhaimin@gmail.com>
2025-04-28 07:41:53 +0200

    Adjusted scripts to only import env vars once

--- scripts.sh
+++ scripts.sh
@@ -1,38 +1,41 @@
 #!/usr/bin/env bash
 
-# ---- Useful constants ----
-PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-export PROJECT_DIR
 
-# shellcheck disable=SC2034
-TRUE=TRUE
-# shellcheck disable=SC2034
-FALSE=FALSE
-# shellcheck disable=SC2034
-SERVER_PROCESS_TAG=SERVER
-# shellcheck disable=SC2034
-UI_PROCESS_TAG=UI
-# shellcheck disable=SC2034
-CLOUDFLARED_PROCESS_TAG=CLOUDFLARED
+if [[ -z "$SETUP_COMPLETE" ]]; then
 
-# ---- Importing all scripts
-for script in "$PROJECT_DIR"/scripts/*.sh; do
-  if [ -f "$script" ]; then
-    # shellcheck disable=SC1090
-    source "$script"
-  fi
-done
+    # ---- Useful constants ----
+    PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    export PROJECT_DIR
 
-# load all color codes
-loadColors
+    # shellcheck disable=SC2034
+    TRUE=TRUE
+    # shellcheck disable=SC2034
+    FALSE=FALSE
+    # shellcheck disable=SC2034
+    SERVER_PROCESS_TAG=SERVER
+    # shellcheck disable=SC2034
+    UI_PROCESS_TAG=UI
+    # shellcheck disable=SC2034
+    CLOUDFLARED_PROCESS_TAG=CLOUDFLARED
 
+    # ---- Importing all scripts
+    for script in "$PROJECT_DIR"/scripts/*.sh; do
+    if [ -f "$script" ]; then
+        # shellcheck disable=SC1090
+        source "$script"
+    fi
+    done
 
-function setup_environment_variables() {
+    # load all color codes
+    loadColors
+
     # Setting up environment variables
     load_env_file "${PROJECT_DIR}"/.env
     load_environment_specific_file "$ENVIRONMENT"
-}
 
+    SETUP_COMPLETE="$TRUE"
+
+fi
 
 main() {
 
@@ -45,16 +48,20 @@ main() {
 
     case $1 in
         start)
-            setup_environment_variables
-            "$PROJECT_DIR"/scripts.sh start-server
-            "$PROJECT_DIR"/scripts.sh start-ui
-            "$PROJECT_DIR"/scripts.sh start-tunnel
+            # shellcheck disable=SC1091
+            . "$PROJECT_DIR"/scripts.sh start-server
+            # shellcheck disable=SC1091
+            . "$PROJECT_DIR"/scripts.sh start-ui
+            # shellcheck disable=SC1091
+            . "$PROJECT_DIR"/scripts.sh start-tunnel
         ;;
         stop)
-            setup_environment_variables
-            "$PROJECT_DIR"/scripts.sh stop-server
-            "$PROJECT_DIR"/scripts.sh stop-ui
-            "$PROJECT_DIR"/scripts.sh stop-tunnel
+            # shellcheck disable=SC1091
+            . "$PROJECT_DIR"/scripts.sh stop-server
+            # shellcheck disable=SC1091
+            . "$PROJECT_DIR"/scripts.sh stop-ui
+            # shellcheck disable=SC1091
+            . "$PROJECT_DIR"/scripts.sh stop-tunnel
         ;;
         start-server)
             setup_backend_venv
